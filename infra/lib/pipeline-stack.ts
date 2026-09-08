c054b06e8f3759bda66e241f1d21a2ed950809c8
vsonti23 <sontivarshik@gmail.com>
2026-09-07 17:10:53 -0700

    infra deployment ci

--- infra/lib/pipeline-stack.ts
+++ infra/lib/pipeline-stack.ts
@@ -22,7 +22,7 @@ export class PipelineStack extends cdk.Stack {
           StringEquals: {
             "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
             "token.actions.githubusercontent.com:sub":
-              "repo:vsonti23/shipyard:ref:refs/heads/main",
+              "repo:vsonti23@173084278/shipyard@1359570164:ref:refs/heads/main",
           },
         },
       ),
